5c59d5614fcca97c4004f1c372cd83adf1407243
ReemaFatima <160411852+ReemaFatima@users.noreply.github.com>
2024-06-09 01:15:57 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+
+console.log ("welcome to code with fatima - cli number guessing game");
+
+const randomNumber = Math.floor(Math.random() * 3 + 1);
+
+const answer = await inquirer.prompt([
+    {
+    name : "userguessednumber",
+    type: "number",
+    message: "Enter your guess number(Number limit from 1 to 5):",
+
+
+},
+
+]);
+
+if (answer.userguessednumber === randomNumber){
+    console.log("congratulation ! you guess a correct number.");
+    }
+    else{
+        console.log("sorry, you guess a wrong number");
+
+    }
